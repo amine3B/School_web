7d065d87eb03925fec8d24aba0e99e2ea58f3186
Amine3B <aminebenzrioual3@gmail.com>
2026-06-03 13:43:23 +0100

    modifier image

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -37,7 +37,7 @@ export default function HeroSection() {
             <div className="relative rounded-2xl overflow-hidden shadow-2xl">
               <div className="aspect-[4/3]">
                 <Image
-                  src="image/Etablicement.webp"
+                  src="image/Etablicement.JPEG"
                   alt="Enfants à l'école"
                   fill
                   className="object-cover"
